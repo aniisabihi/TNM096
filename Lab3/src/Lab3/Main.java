52b67bd1fd9dbc27456f7d65d4d7f93748aa005f
Tyddan <amanda.tyden@gmail.com>
2019-05-03 15:07:02 +0200

    lab 3 påbörjad

---
+++ Lab3/src/Lab3/Main.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+    	String[] formulae = {"a V -b", "a V b", "c"};
+    	Engine theEngine = new Engine(formulae);
+    	
+    	
+    }
+}
